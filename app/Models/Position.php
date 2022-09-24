7e71ad39a6f2dc2d6a0751495826782e7d99d62f
Ted30 <teddymarcenaje@gmail.com>
2022-09-24 19:11:02 +0800

    added relationship to department

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -13,4 +13,9 @@ class Position extends Model
         "position",
         "department_id"
     ];
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class, 'department_id');
+    }
 }
