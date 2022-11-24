178cca3061ffa58e78f3844bbf3cb11b3c9295a9
Ted30 <teddymarcenaje@gmail.com>
2022-11-25 00:05:40 +0800

    added payroll migration

---
+++ database/migrations/2022_11_24_144058_create_payrolls_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePayrollsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payrolls', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('position_id')
+                ->constrained();
+            $table->float('gross_amount');
+            $table->float('total_deduction');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payrolls');
+    }
+}
