20e36e33977c12086023df541ce59550eb1e7b97
Ted30 <teddymarcenaje@gmail.com>
2022-11-28 02:49:14 +0800

    added account create

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use App\Models\Employee;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class UserController extends Controller
 {
-    protected function store(Request $request) {
+    protected function store(Request $request)
+    {
 
         $formFields = $request->validate([
-            'work_start'=>['required' , 'string'],
-            'work_end'=>['required', 'string'],
+            'work_start' => ['required', 'string'],
+            'work_end' => ['required', 'string'],
             'first_name' => ['required', 'string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
             'address' => ['required', 'string', 'max:255'],
@@ -21,19 +24,21 @@ class UserController extends Controller
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'contact_number' => ['required'],
             'password' => ['required', 'string', 'min:8', 'confirmed']
-         ]);
+        ]);
 
-         if ($request->hasFile('photo')) {
+        if ($request->hasFile('photo')) {
             $formFields['photo'] = $request->file('photo')->store('profile', 'public');
-         }
+        }
 
-         $formFields['password'] = bcrypt($formFields['password']);
+        $formFields['password'] = bcrypt($formFields['password']);
 
-         $user = Employee::create($formFields);
+        $user = Employee::create(Arr::except($formFields, ['password']));
 
-         auth()->login($user);
+        $account = Account::create(["employee_id" => $user->id, "password" => $formFields['password'], "role_id" => null]);
 
-         return redirect('/home');
 
+        auth()->login($user);
+
+        return redirect('/home');
     }
 }
