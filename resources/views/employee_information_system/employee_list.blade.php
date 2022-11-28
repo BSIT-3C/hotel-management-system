2d705756a5586e810b72dd111e49ec2133d89e68
John Otilla <johncanila.otilla@bicol-u.edu.ph>
2022-11-28 11:39:42 +0800

    Update file upload and minor changesin employee table

--- resources/views/employee_information_system/employee_list.blade.php
+++ resources/views/employee_information_system/employee_list.blade.php
@@ -15,26 +15,6 @@
             </div>
         </div>
 
-        {{-- SEARCH 
-            <div class="d-flex justify-content-center m-auto search mb-4">
-                <div class="pe-2">
-                    <form class="d-flex" method="post" action="">
-                        <label for="search" class="form-label pe-3 mt-1"><strong>Search</strong></label>
-                        <input type="search" class="form-control shadow-sm" id="search" style="width: 700px; height: auto;">
-                    </form>
-                </div>
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle text-primary" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="background: none; border: none;">
-                      Filter <i class="fa-solid fa-filter"></i>
-                    </button>
-                    <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="#">Actions</a></li>
-                      <li><a class="dropdown-item" href="#">Another action</a></li>
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                  </div>
-            </div>--}}
-
             <div class="dtr_Tables rounded p-2">
                 <table id="table" class="table table-hover table-primary table-striped text-center shadow-sm">
                     <thead>
@@ -46,7 +26,6 @@
                         <th scope="col">Birthday</th>
                         <th scope="col">Email</th>
                         <th scope="col">Contact #</th>
-                        <th scope="col"></th>
                         <th scope="col"></th>
                       </tr>
                     </thead>
@@ -63,18 +42,13 @@
                           <td>{{$List->birthday}}</td>
                           <td>{{$List->email}}</td>
                           <td>{{$List->contact_number}}</td>
-                          <td><a href="/home/edit/{{$List->id}}"><i class="fa-solid fa-pen-to-square text-primary"></i></a></td>
-                          <td>
-                            <form action="/home/delete/{{$List->id}}" method="POST">
-                              @csrf
-                              @method('DELETE')
-                              <button type="submit" style="background: none; border: none;"><i class="fa-solid fa-trash text-danger"></i></button>
-                              {{-- <a ><i class="fa-solid fa-trash text-danger"></i></a> --}}
-                            </form>
-                          </td>
-                          {{-- <form id="delete-form" action="/home/delete/{{$List->id}}" method="DELETE" class="d-none">
+                          <td class="d-flex"><a href="/home/edit/{{$List->id}}"><i class="fa-solid fa-pen-to-square text-primary pe-3"></i></a>
+                          <form action="/home/delete/{{$List->id}}" method="POST">
                             @csrf
-                          </form> --}}
+                            @method('DELETE')
+                            <button type="submit" style="background: none; border: none;"><i class="fa-solid fa-trash text-danger"></i></button>
+                          </form>
+                          </td>
                         </tr>
                       @endforeach
                         
