4101280183a31c519c5176128cbb742831b50ace
-Haru- <52265509+traxex123lord@users.noreply.github.com>
2022-12-08 01:21:51 +0800

    added updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,7 @@
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link href="{{ URL::asset('css/homepage.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ URL::asset('css/homepage.scss') }}" rel="stylesheet" type="text/css" >
     <style>
         body {
             font-family: 'Nunito', sans-serif;
@@ -47,47 +48,33 @@
     </div>
     <main data-scroll-container>
         <div class="wrap" data-scroll-section>
-            <section class="section home" data-block-section="1" id="home">
-                <div class="row">
-                    <div class="row">
-                        <div class="col-12">
-                            <h3>Welcome to tranquil</h3>
-                            <h5><i>Where one's comfort begins...</i></h5>
-                        </div>
-                        <div class="col-12">
-                            <div class="row">
-                                    @if (Route::has('login'))
-                                        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                                            @auth
-                                                <a href="{{ url('/home') }}">
-                                                    <div class="homepageButton"> 
-                                                        Home
-                                                    </div> 
-                                                </a>
-                                            @else
-                                            <div class="col-12 mt-2 mb-2">
-                                                <a href="{{ route('login')}}">
-                                                    <div class="homepageButton"> 
-                                                        Login
-                                                    </div>
-                                                </a>
-                                            </div>
-                                            @if (Route::has('register'))
-                                                <a href="{{ route('register') }}">
-                                                    <div class="col-12">
-                                                        <div class="homepageButton">
-                                                            Register
-                                                        </div>
-                                                    </div>
-                                                </a>
-                                            @endif
-                                            @endauth
-                                        </div>
-                            </div>
-                        </div>
+            <section class="section home" data-block-section="1" id="home" style="background-image:url('{{asset("assets/mainBG.jpg")}}');background-size:100%;color:#FFFF";>
+                <div class="container m-5">
+                    <div class="d-flex justify-content-center">
+                        <h1>Welcome to <i><b>Tranquil</b></i></h1>
                     </div>
+                    <div class="d-flex justify-content-center">
+                        <h4><i>Where one's comfort begins...</i></h4>
+                    </div>
+                                @if (Route::has('login'))
+                                    @auth
+                                        <a href="{{ url('/home') }}">Home</a>
+                                @else
+                                    <a href="{{ route('login') }}">
+                                        <div class="d-flex justify-content-center m-3"> 
+                                            <button type="button" class="btn btn-primary">Login</button>
+                                        </div>
+                                    </a>
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}">
+                                        <div class="d-flex justify-content-center">
+                                            <button type="button" class="btn btn-primary">Register</button>
+                                        </div>
+                                    </a>
+                                @endif
+                                    @endauth
+                                @endif
                 </div>
-            @endif
             </section>
             <section class="section collection" data-block-section="2" id="collection">
                 images/features/etc. go here
