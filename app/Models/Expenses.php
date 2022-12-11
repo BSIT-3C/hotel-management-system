a20a65466d4af710b7ea7f3710ad6d1c3b39177e
Ted30 <teddymarcenaje@gmail.com>
2022-12-12 00:48:57 +0800

    Expenses model, factory, migration & seeder

---
+++ app/Models/Expenses.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Expenses extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        "type",
+        "name",
+        "unit_price",
+        "date",
+    ];
+}
