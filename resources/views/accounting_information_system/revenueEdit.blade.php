d44d8112943e90fa745abf136fdd459561849230
Marriane Natad <marrianejanenatad@bicol-u.edu.ph>
2022-12-13 23:55:02 +0800

    removed subcolumn

--- resources/views/accounting_information_system/revenueEdit.blade.php
+++ resources/views/accounting_information_system/revenueEdit.blade.php
@@ -14,6 +14,13 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
 
+    <!-- jQuery -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+
 <style>
 
 body {
@@ -85,34 +92,10 @@ table {
         }
 
         thead td:nth-child(6) {
-            grid-column: 4;
-        }
-
-        thead td:nth-child(7) {
-            grid-column: 5;
-        }
-
-        thead td:nth-child(8) {
-            grid-column: 6;
-        }
-
-        thead td:nth-child(9) {
-            grid-column: 7;
-        }
-
-        thead td:nth-child(10) {
-            grid-column: 8;
-        }
-
-        thead td:nth-child(11) {
-            grid-column: 9;
-        }
-
-        thead td:nth-child(12) {
             grid-column: 10;
         }
 
-        thead td:nth-child(13) {
+        thead td:nth-child(7) {
             grid-column: 11;
         }
 
@@ -161,25 +144,8 @@ table {
             grid-row: 1/3;
         }
 
-.column {
-    float: left;
-    padding: 1px;
-    width:70%;
-    margin-left: 200px;
-    margin-top: 5px;
-}
-
-.total {
-    font-size: 10px;
-    font-family: 'Times New Roman', Times, serif;
-    border-collapse: collapse;
-    box-shadow: 0 10px 10px 0 #DFDFDF;
-    background-color: #DFDFDF;
-}
-
 </style>
 
-</head>
   <body>
     <nav class="navbar navbar-expand-md">
         <img src="https://drive.google.com/uc?export=download&id=1N-yj2KqeeyVd3t_RLgeaO2HGI5i94h4P" alt="logo3" width="80" height="40">
@@ -206,15 +172,6 @@ table {
                     <td id="b">Superior</td>
                     <td id="c">Luxury</td>
                     <td id="d">Suite</td>
-                    <td>1</td>
-                    <td>2</td>
-                    <td>3</td>
-                    <td>1</td>
-                    <td>2</td>
-                    <td>3</td>
-                    <td>1</td>
-                    <td>2</td>
-                    <td>3</td>
                     <td class="club">Club Room</td>
                     <td class="col_total">Total</td>
                 </tr>
@@ -225,9 +182,11 @@ table {
                     <td></td>
                     <td></td>
                     <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td class="cell_club"></td>
+                    <td class="cell_total"></td>
+                </tr>
+                <tr>
+                    <td class="cell_date"></td>
                     <td></td>
                     <td></td>
                     <td></td>
@@ -239,9 +198,11 @@ table {
                     <td></td>
                     <td></td>
                     <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td class="cell_club"></td>
+                    <td class="cell_total"></td>
+                </tr>
+                <tr>
+                    <td class="cell_date"></td>
                     <td></td>
                     <td></td>
                     <td></td>
@@ -253,97 +214,9 @@ table {
                     <td></td>
                     <td></td>
                     <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
                     <td class="cell_club"></td>
                     <td class="cell_total"></td>
                 </tr>
-                <tr>
-                    <td class="cell_date"></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="cell_club"></td>
-                    <td class="cell_total"></td>
-                </tr>
-                <tr>
-                    <td class="cell_date"></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="cell_club"></td>
-                    <td class="cell_total"></td>
-                </tr>
-                <tr>
-                    <td class="cell_date"></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="cell_club"></td>
-                    <td class="cell_total"></td>
-                </tr>
-                <tr>
-                    <td class="cell_date"></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="cell_club"></td>
-                    <td class="cell_total"></td>
-                </tr>
-                <tr>
-                    <td class="cell_date"></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="cell_club"></td>
-                    <td class="cell_total"></td>
-                </tr>
-            </tbody>
-        </table>
-
-        <div class="column total">
-            <h2><b>TOTAL:</b></h2>
-        </div>
     </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery -->
-    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
