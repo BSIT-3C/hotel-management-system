c637974aa288a82685e7f57c00565eaf8b2c9c2c
Marriane Natad <marrianejanenatad@bicol-u.edu.ph>
2022-12-14 15:51:22 +0800

    edited removed subcol

--- resources/views/accounting_information_system/revenueEdit.blade.php
+++ resources/views/accounting_information_system/revenueEdit.blade.php
@@ -1,24 +1,34 @@
+
+@php
+    $i = '';
+@endphp
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <title>RevenueEdit</title>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="main.css">
+    <!-- FOR JQUERY AND DATEPICKER  -->
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <!-- <link rel="stylesheet" href="/resources/demos/style.css"> -->
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.13.1/b-2.3.3/cr-1.6.1/date-1.2.0/fc-4.2.1/fh-3.3.1/r-2.4.0/rg-1.3.0/sp-2.1.0/datatables.min.css"/>
 
-    <!-- Bootstrap Date-Picker Plugin -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.13.1/b-2.3.3/cr-1.6.1/date-1.2.0/fc-4.2.1/fh-3.3.1/r-2.4.0/rg-1.3.0/sp-2.1.0/datatables.min.js"></script>
 
-    <!-- jQuery -->
-    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- FOR BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+        crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 </head>
 
 <style>
@@ -57,66 +67,17 @@ table {
             background-color: #E1DFD2;
         }
 
-        table thead tr {
-            display: grid;
-            grid-template-columns: repeat(15, 1fr);
-
-        }
-
         table tbody tr td {
             padding: 15px;
-        }
-
-        thead td:nth-child(1) {
-            grid-column: 1/3;
-            grid-row: 1/3;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-
-        thead td:nth-child(2) {
-            grid-column: 3/6;
-        }
-
-        thead td:nth-child(3) {
-            grid-column: 6/9;
-        }
-
-        thead td:nth-child(4) {
-            grid-column: 9/12;
-        }
-
-        thead td:nth-child(5) {
-            grid-column: 3;
-        }
-
-        thead td:nth-child(6) {
-            grid-column: 10;
-        }
-
-        thead td:nth-child(7) {
-            grid-column: 11;
         }
 
         tbody tr td {
             border: 1px solid black;
         }
 
-        tbody tr {
-            display: grid;
-            grid-template-columns: repeat(15, 1fr);
-            text-align: center;
-
-        }
-
         .club {
-            display: flex;
-            align-items: center;
-            justify-content: center;
+            grid-column: 1/3;
 
-            grid-column: 12/14;
-            grid-row: 1/3;
         }
 
         .cell_date {
@@ -124,24 +85,9 @@ table {
             grid-column: 1/3;
         }
 
-        .cell_club {
-
-            grid-column: 12/14;
-        }
-
-        .cell_total {
-
-            grid-column: 14/16;
-        }
-
         .col_total {
-            display: flex;
-            align-items: center;
-            justify-content: center;
+            grid-column: 1/3;
 
-            text-align: center;
-            grid-column: 14/16;
-            grid-row: 1/3;
         }
 
 </style>
@@ -149,7 +95,6 @@ table {
   <body>
     <nav class="navbar navbar-expand-md">
         <img src="https://drive.google.com/uc?export=download&id=1N-yj2KqeeyVd3t_RLgeaO2HGI5i94h4P" alt="logo3" width="80" height="40">
-            <span class="navbar-toggler-icon"></span>
     </nav>
 
     <h1><b> REVENUE</b></h1>
@@ -165,7 +110,7 @@ table {
     <div class="container">
         Latest Update...
 
-        <table>
+        <table id="revenue">
             <thead>
                 <tr>
                     <td id="a">Date</td>
@@ -178,45 +123,35 @@ table {
             </thead>
             <tbody>
                 <tr>
-                    <td class="cell_date"></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="cell_club"></td>
-                    <td class="cell_total"></td>
+                    <td id="a"></td>
+                    <td id="b"></td>
+                    <td id="c"></td>
+                    <td id="d"></td>
+                    <td class="club"></td>
+                    <td class="col_total"></td>
                 </tr>
                 <tr>
-                    <td class="cell_date"></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="cell_club"></td>
-                    <td class="cell_total"></td>
+                    <td id="a"></td>
+                    <td id="b"></td>
+                    <td id="c"></td>
+                    <td id="d"></td>
+                    <td class="club"></td>
+                    <td class="col_total"></td>
                 </tr>
                 <tr>
-                    <td class="cell_date"></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="cell_club"></td>
-                    <td class="cell_total"></td>
+                    <td id="a"></td>
+                    <td id="b"></td>
+                    <td id="c"></td>
+                    <td id="d"></td>
+                    <td class="club"></td>
+                    <td class="col_total"></td>
                 </tr>
-                <tr>
-                    <td class="cell_date"></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="cell_club"></td>
-                    <td class="cell_total"></td>
-                </tr>
-                <tr>
-                    <td class="cell_date"></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="cell_club"></td>
-                    <td class="cell_total"></td>
-                </tr>
+            </tbody>
+        </table>
     </div>
-</body>
+  </body>
 </html>
+
+<script>
+     $('#revenue').DataTable();
+</script>
