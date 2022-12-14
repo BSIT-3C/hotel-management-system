e8b779b2df75d70fce640dbb1664a751b5c26086
mathias19411 <bermejomathiasjohnoliver@gmail.com>
2022-12-13 19:10:05 -0800

    added housekeeping backend

--- app/Http/Controllers/HousekeepingController.php
+++ app/Http/Controllers/HousekeepingController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lost_And_Found_Item;
+use App\Models\Room;
+use App\Models\User;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 
 class HousekeepingController extends Controller
@@ -14,6 +18,83 @@ class HousekeepingController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+    }
+
+    //Manage Room Status
+    public function manage_rooms()
+    {
+        $rooms = Room::all();
+
+        return view('/housekeeping/manage'); 
+    }
+
+    //View Rooms
+    public function show_rooms()
+    {
+        $rooms = Room::all();
+
+        return view('/housekeeping/viewall', [
+            'view_rooms' => $rooms
+        ]); 
+    }
+
+    //Lost and Found
+    public function show_lostandfound()
+    {
+        $lfitems = Lost_And_Found_Item::all();
+
+        return view('/housekeeping/lostandfound', [
+            'items' => $lfitems
+        ]); 
+    }
+
+    public function create_lostandfound(Request $request)
+    {
+        $item = new Lost_And_Found_Item();
+        $item -> item_name = $request -> input('item_name');
+        $item -> item_quantity = $request -> input('item_quantity');
+        $item -> room_no_found_at = $request -> input('room_number');
+        $item -> item_status = $request -> input('item_status');
+        $item -> date_and_time = $request -> input('date_and_time');
+        $item -> save();
+
+        $lfitems = Lost_And_Found_Item::all();
+
+        return view('/housekeeping/lostandfound', [
+            'items' => $lfitems , 'message' => 'The Item Was Successfully Added!'
+        ]);
+    }
+
+    public function update_lostandfound($id)
+    {
+        $lfitems = Lost_And_Found_Item::find($id);
+
+        return view('/housekeeping/lostandfound_update', [
+            'items' => $lfitems
+        ]); 
+    }
+
+    public function process_update_lostandfound(Request $request, $id)
+    {
+        $lfitems = Lost_And_Found_Item::find($id);
+
+        $lfitems-> item_status = $request -> input('item_status');
+
+        $lfitems -> save();
+
+        $lfitems = Lost_And_Found_Item::all();
+
+        return view('/housekeeping/lostandfound', [
+            'items' => $lfitems , 'message' => 'The Item Status was Successfully Updated!'
+        ]);
+    }
+
+    //Assign Housekeeper
+    public function assign_housekeeper()
+    {
+        //$rooms = Room::all();
+
+        return view('/housekeeping/assign'); 
     }
 
     /**
--- resources/views/housekeeping/viewall.blade.php
+++ resources/views/housekeeping/viewall.blade.php
@@ -5,18 +5,50 @@
     <a href="/housekeeping/home" class="inline-block text-black ml-4 mb-4">
         <button class="btn btn-light mb-2"><i class="fa-solid fa-arrow-left"></i></i> Back</button>
     </a>
-    <div class="card border-primary mb-3" style="max-width: 100%; height: 540px; margin-left: 10px; border-radius: 15px;">
-        <div class="container">
-            <h1>Rooms List</h1>
-            <table border="1">
-                <tr>
-                    <td>ID</td>
-                    <td>Employee ID</td>
-                    <td>Employee Name</td>
-                    <td>Employee Position</td>
-                    <td>Employee Status</td>
-                </tr>
-            </table>
+    <div class="card border-primary mb-3" style="max-width: 100%; height: 520px; margin-left: 10px; border-radius: 15px;">
+        <div class="row">
+            <div class="card">
+                <div class="card-header">
+                    Room List
+                </div>
+                <div class="card-body">
+                    <table class="table table-striped table-bordered" id="list">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Room Number</th>
+                                <th>Room Type</th>
+                                <th>Room Status</th>
+                                <th>Timestamp</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @unless (count($view_rooms) == 0)
+                                @foreach ($view_rooms as $room)
+                                    <tr>
+                                        <td>{{$room['id']}}</td>
+                                        <td>{{$room['room_number']}}</td>
+                                        <td>{{$room['room_type_id']}}</td>
+                                        <td>{{$room['room_status_id']}}</td>
+                                        <td>{{$room['updated_at']}}</td>
+                                    </tr>
+                                @endforeach
+                            @else
+                                <tr>
+                                  <td colspan="7">No Employee Records</td>
+                                </tr>
+                            @endunless
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
 @endsection
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('#list').DataTable();
+        });
+    </script>
