f2b6cbd3f8ddd62ff52885859aa7122faf20aca5
scooBIENdoo <reubien.1018@gmail.com>
2022-12-17 11:38:58 +0800

    assign a housekeeper initial changes

--- resources/views/housekeeping/viewall.blade.php
+++ resources/views/housekeeping/viewall.blade.php
@@ -47,7 +47,7 @@
                 </div>
             </div>
         </div>
-    </div><br>
+    </div><br><br>
 @endsection
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
